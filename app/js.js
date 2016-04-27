e847d36479f855eb58c0ab95266f3684592765e1
Ke Nguyen Phu <kenguyen276@gmail.com>
2016-04-27 17:39:50 +0700

    Add Project sections, add some personal information...

--- app/js.js
+++ app/js.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
     $("#show-info").click(function(){
-        $("#contact-info-container").toggle();
+        $("#contact-info-container").slideToggle();
     });
 });
