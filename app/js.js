449e558d061299b874559d5012bbc41a1ba54cd7
Ke Nguyen Phu <kenguyen276@gmail.com>
2016-05-04 23:11:42 +0700

    Add edit form to each section

--- app/js.js
+++ app/js.js
@@ -2,4 +2,74 @@ $(document).ready(function(){
     $("#show-info").click(function(){
         $("#contact-info-container").slideToggle();
     });
+    
+    $(".edit-name").click(function(){
+        $("#pop-up-edit-name").show();
+    });
+    
+    $(".btn-cancel").click(function(){
+        $(".pop-up").hide();
+    });
+    
+    $(".edit-headline").click(function(){
+        $("#pop-up-edit-headline").show();
+    });
+    
+    $(".edit-location-industry").click(function(){
+        $("#pop-up-edit-location-industry").show();
+    });
+    
+    $(".edit-education").click(function(){
+        $('html,body').animate({
+        scrollTop: $("#background-education-container").offset().top},
+        'fast');
+    });
+    
+    $(".edit-job").click(function(){
+        $('html,body').animate({
+        scrollTop: $("#background-experience-container").offset().top},
+        'fast');
+    });
+    
+    $(".edit-summary").click(function(){
+        $(".summary-content>p").hide();
+        $(".edit-summary-container").show();
+    });
+    
+    $(".edit-summary-container .btn-cancel").click(function(){
+        $(".summary-content>p").show();
+        $(".edit-summary-container").hide();
+    });
+    
+    $("#btn-add-position").click(function(){
+        $(".add-position-container").show();
+    });
+    
+    $("#btn-cancel-add-position").click(function(){
+        $(".add-position-container").hide();
+    });
+    
+    $("#btn-add-education").click(function(){
+        $(".add-education-container").show();
+    });
+    
+    $("#btn-cancel-add-education").click(function(){
+        $(".add-education-container").hide();
+    });
+    
+    $("#btn-add-skill").click(function(){
+        $(".add-skill-container").show();
+    });
+    
+    $("#btn-cancel-add-skill").click(function(){
+        $(".add-skill-container").hide();
+    });
+    
+    $("#btn-add-project").click(function(){
+        $(".add-project-container").show();
+    });
+    
+    $("#btn-cancel-add-project").click(function(){
+        $(".add-project-container").hide();
+    });
 });
